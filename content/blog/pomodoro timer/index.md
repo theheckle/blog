3650832747bc58da249959b5faed3c8107d68f53
Sam Heckle <samheckle@yahoo.com>
2020-08-30 20:26:45 -0700

    made basic theme

---
+++ content/blog/pomodoro timer/index.md
@@ -0,0 +1,16 @@
+---
+title: pomodoro timer
+date: "2020-08-30"
+---
+
+this was an exploration into discord bots, and my first time making one. in a nutshell, check out [my repository](https://github.com/theheckle/tanatimer) and [add this to your discord](bit.ly/2ebj3d2)! if you want to see a new feature, please make an issue and i will try to get to it. 
+
+if you don't know what the pomodoro method is, you might want to [read up on it](https://en.wikipedia.org/wiki/Pomodoro_Technique). effectively, it is chunking out time so that you can work more effectively. most of the time you work for 25 minutes and then take a 5 minute break. so, i built a discord bot to help me with that.
+
+this blog assumes you have no familiarity with discord bots, so i will start with the basics (read: this was my first discord bot).
+
+tools you will need:\
+[discord.js](https://www.npmjs.com/package/discord.js)
+```
+npm install --save discord.js
+```
---
+++ gatsby-config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  siteMetadata: {
+    title: `fridge notes`,
+    author: {
+      name: `sam heckle`,
+      summary: `class of itp 2022. doing shit.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `http://blog.samheckle.com`,
+    social: {
+      twitter: `the_heckle`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/favicon.ico`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
+}
